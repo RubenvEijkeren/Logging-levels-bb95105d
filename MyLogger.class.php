f753b0bbb0c88b10b46a64938d69934a57bf5a3d
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2020-06-19 12:04:48 +0200

    Uitwerking van de exercise

--- MyLogger.class.php
+++ MyLogger.class.php
@@ -5,10 +5,29 @@
 class MyLogger
 {
     
-    public function log($message){
-        echo $message;
+    public function log($message, $level)
+    {
+        echo $level . ": " . $message;
+    }
+    public function warning($message)
+    {
+        $this->log($message, "WARNING");
+    }
+    public function error($message)
+    {
+        $this->log($message, "ERROR");
+    }
+    public function info($message)
+    {
+        $this->log($message, "INFO");
+    }
+    public function debug($message)
+    {
+        $this->log($message, "DEBUG");
     }
 }
 $logger = new MyLogger();
-$logger->log("Hello world!");
+$logger->error("dit is een error");
+$logger->log("Hello world!", "INFO");
+$logger->info("this is an info log");
 ?>
